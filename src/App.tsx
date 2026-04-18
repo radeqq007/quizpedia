76f8b299857934c24d5f0208c9b025c91f853719
radeqq007 <radeqq007@interia.pl>
2026-04-18 18:27:27 +0200

    feat: add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
+import { NotFound } from './components/pages/404';
 import { Home } from './components/pages/Home';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
