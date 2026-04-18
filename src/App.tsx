419ba70174d24cc2470e34c2719b2193858705c9
radeqq007 <radeqq007@interia.pl>
2026-04-19 00:23:43 +0200

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { NotFound } from "@/pages/404";
 import { Home } from "@/pages/Home";
 import { Route, Routes } from "react-router-dom";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,25 @@
+export const Footer = () => {
+  return (
+    <div className="absolute bottom-0 left-0 right-0 h-10 py-10 px-4 text-secondary-foreground flex justify-between gap-6 *:flex *:gap-4">
+      <span>
+        © 2026
+        <span className="flex gap-1">
+          Made with ❤ by
+          <a href="https://github.com/radeqq007" target="_blank" className="underline">
+            Radosław Kaczmarczyk
+          </a>
+        </span>
+      </span>
+
+      <span>
+        <a href="https://github.com/radeqq007/quizpedia" target="_blank" className="underline">
+          Github
+        </a>
+
+        <a href="mailto:radoslaw.kaczmarczyk@proton.me" className="underline">
+          Contact
+        </a>
+      </span>
+    </div>
+  )
+}
