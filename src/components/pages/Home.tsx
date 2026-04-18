f5c8e288e864a71f5adba68b917edbfbc752b265
radeqq007 <radeqq007@interia.pl>
2026-04-18 20:54:00 +0200

    feat: add logo

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,3 +1,4 @@
+import logo from "@/assets/logo.svg";
 import { Button } from "@/components/ui/button";
 import { Field, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
@@ -19,7 +20,10 @@ export const Home = () => {
 
   return (
     <div className="flex flex-col gap-18 items-center w-1/3 m-auto h-screen p-8">
-      <h1 className="text-6xl font-black">Quizpedia</h1>
+      <span className="flex items-end gap-2">
+        <img src={logo} className="h-12" alt="logo" />
+        <h1 className="text-6xl font-black">Quizpedia</h1>
+      </span>
 
       <form className="w-full flex items-end gap-5" onSubmit={handleSearch}>
         <Field>
