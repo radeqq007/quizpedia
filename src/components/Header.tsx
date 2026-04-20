521a00d6f35be1e3628a05c93342cd2867e685ce
radeqq007 <radoslaw.kaczmarczyk@proton.me>
2026-04-20 08:18:59 +0200

    feat: make the header navigate to / on click

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,14 @@
 import logo from "@/assets/logo.svg";
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
+  const navigate = useNavigate();
+
   return (
-    <span className="flex items-end gap-2">
+    <span
+      onClick={() => navigate("/")}
+      className="flex items-end gap-2 cursor-pointer"
+    >
       <img src={logo} className="h-12" alt="logo" />
       <h1 className="text-6xl font-black">Quizpedia</h1>
     </span>
