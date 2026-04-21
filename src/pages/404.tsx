627c56ce75974181878adf350981e127bd4d6ed9
radeqq007 <radeqq007@interia.pl>
2026-04-21 15:05:57 +0200

    style: update the 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,7 @@ import cat from "@/assets/cat.gif";
 
 export const NotFound = () => {
   return (
-    <div className="flex items-center justify-center h-screen w-screen flex-col">
+    <div className="flex items-center justify-center flex-col">
       <img src={cat} alt="404 not found" className="w-60" />
       <h1 className="text-8xl font-black">404</h1>
       <p className="text-2xl">Page not found.</p>
