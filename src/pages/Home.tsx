fee1ea06cd656874e548a21dba2ea4e4d97c4daf
radeqq007 <radeqq007@interia.pl>
2026-04-21 15:26:40 +0200

    style: improve the summary style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -46,7 +46,7 @@ export const Home = () => {
     <div className="flex flex-col gap-18 items-center w-full md:w-2/3 lg:w-1/3 lg:min-w-140 m-auto p-8">
       <SearchBar selected={selected} onSelect={setSelected} />
 
-      <div className="border border-input bg-transparent rounded-md px-6 py-4 w-full min-h-40 flex flex-col gap-3">
+      <div className="border border-input bg-transparent rounded-md px-6 py-4 w-full min-h-40 flex flex-col justify-between gap-3">
         <Label className="text-2xl font-bold">Summary</Label>
         {summary}
 
