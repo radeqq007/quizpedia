355b0190b29575662fe9a411bae913f23bef7a57
radeqq007 <radeqq007@interia.pl>
2026-04-21 13:28:26 +0200

    refactor: move the search bar into a seperate component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,156 @@
+import { Field, FieldLabel } from "@/components/ui/field";
+import {
+  InputGroup,
+  InputGroupButton,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import {
+  Popover,
+  PopoverAnchor,
+  PopoverContent,
+} from "@/components/ui/popover";
+import { Spinner } from "@/components/ui/spinner";
+import { useWikipediaSearch } from "@/lib/wikipedia";
+import { LucideSearch } from "lucide-react";
+import { AnimatePresence, motion } from "motion/react";
+import { useEffect, useState } from "react";
+
+interface SearchBarProps {
+  selected: string;
+  onSelect: (value: string) => void;
+}
+
+const placeholders: string[] = [
+  "Ducks",
+  "Radiohead",
+  "The smashing pumpkins",
+  "Cats",
+  "Edam cheese",
+  "Earth",
+  "Van Halen",
+  "Linus Torvalds",
+  "Linux kernel",
+  "Jimi Hendrix",
+  "Minecraft",
+  "Time travel",
+  "Quantum physics",
+  "Roman empire",
+  "Area 51",
+  "Evolution",
+  "Vikings",
+  "Game development",
+  "Artificial intelligence",
+  "Black holes",
+  "X Japan",
+  "Neon Genesis Evangelion",
+  "Pink Floyd",
+  "Metallica",
+  "Angine de Poitrine",
+  "Weezer",
+  "Black Sabbath",
+  "Bocchi the Rock",
+  "88Kasyo Junrei",
+];
+
+export const SearchBar = ({ onSelect, selected }: SearchBarProps) => {
+  const [input, setInput] = useState<string>("");
+  const [query, setQuery] = useState<string>("");
+
+  const { data: searchData, isFetching: isSearching } =
+    useWikipediaSearch(query);
+
+  const [searchOpen, setSearchOpen] = useState<boolean>(false);
+  const searchResults: string[] = searchData?.[1];
+
+  const handleSelect = (value: string, e?: React.SubmitEvent) => {
+    e?.preventDefault();
+    if (!value && searchResults.length === 0) return;
+    if (!value) return;
+
+    onSelect(value);
+    setInput(value);
+    setSearchOpen(false);
+  };
+
+  useEffect(() => {
+    if (!input) {
+      setSearchOpen(false);
+      setQuery("");
+      return;
+    }
+
+    if (input === selected) {
+      setSearchOpen(false);
+      return;
+    }
+
+    const delay = setTimeout(() => {
+      setQuery(input);
+      setSearchOpen(true);
+    }, 500);
+
+    return () => clearTimeout(delay);
+  }, [input]);
+
+  return (
+    <form
+      className="w-full flex items-end gap-5"
+      onSubmit={(e) => handleSelect(searchResults[0], e)}
+    >
+      <Field>
+        <span className="flex flex-col gap-2">
+          <FieldLabel className="text-xl" htmlFor="input">
+            I want a quiz about...
+          </FieldLabel>
+          <Popover open={searchOpen} onOpenChange={setSearchOpen}>
+            <PopoverAnchor asChild>
+              <InputGroup>
+                <InputGroupInput
+                  id="input"
+                  autoComplete="off"
+                  placeholder={`${placeholders[Math.floor(Math.random() * placeholders.length)]}...`}
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+                />
+
+                <InputGroupButton
+                  onClick={() => handleSelect(searchResults[0])}
+                >
+                  {isSearching ? <Spinner /> : <LucideSearch />}
+                </InputGroupButton>
+              </InputGroup>
+            </PopoverAnchor>
+
+            <PopoverContent
+              onOpenAutoFocus={(e) => e.preventDefault()}
+              className="p-0 mt-0 w-(--radix-popover-trigger-width)"
+            >
+              <AnimatePresence mode="wait">
+                <motion.div
+                  initial={{ opacity: 0, y: -5 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.1 }}
+                  className="w-full flex flex-col rounded-lg px-1 py-2"
+                >
+                  {searchResults && searchResults.length > 0 ? (
+                    searchResults?.map((result) => (
+                      <button
+                        className="my-1 cursor-pointer hover:bg-input h-8 px-3 rounded-lg text-left transition-colors"
+                        key={result}
+                        onClick={() => handleSelect(result)}
+                      >
+                        {result}
+                      </button>
+                    ))
+                  ) : (
+                    <span className="w-full text-center">No results.</span>
+                  )}
+                </motion.div>
+              </AnimatePresence>
+            </PopoverContent>
+          </Popover>
+        </span>
+      </Field>
+    </form>
+  );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,44 +1,24 @@
+import { SearchBar } from "@/components/SearchBar";
 import { Button } from "@/components/ui/button";
-import { Field, FieldLabel } from "@/components/ui/field";
-import {
-  InputGroup,
-  InputGroupButton,
-  InputGroupInput,
-} from "@/components/ui/input-group";
 import { Label } from "@/components/ui/label";
-import {
-  Popover,
-  PopoverAnchor,
-  PopoverContent,
-} from "@/components/ui/popover";
 import { Skeleton } from "@/components/ui/skeleton";
-import { Spinner } from "@/components/ui/spinner";
 import { useQuiz } from "@/lib/quiz";
 import { useQuizStore } from "@/lib/store";
-import { useWikipediaArticle, useWikipediaSearch } from "@/lib/wikipedia";
-import { LucideSearch } from "lucide-react";
-import { AnimatePresence, motion } from "motion/react";
-import { useEffect, useState } from "react";
+import { useWikipediaArticle } from "@/lib/wikipedia";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
-  const [input, setInput] = useState<string>("");
-  const [query, setQuery] = useState<string>("");
   const [selected, setSelected] = useState<string>("");
-  const { data: searchData, isFetching: isSearching } =
-    useWikipediaSearch(query);
   const { data: articleData } = useWikipediaArticle(selected);
   const {
     data: quizData,
     isFetching: isGenerating,
     error: quizError,
   } = useQuiz(articleData);
-  const searchResults: string[] = searchData?.[1];
 
   const navigate = useNavigate();
   const setQuiz = useQuizStore((s) => s.setQuiz);
-
-  const [searchOpen, setSearchOpen] = useState<boolean>(false);
 
   let summary;
   if (isGenerating) {
@@ -62,127 +42,9 @@ export const Home = () => {
     summary = quizData?.summary;
   }
 
-  const handleSelect = (value: string, e?: React.SubmitEvent) => {
-    e?.preventDefault();
-    if (searchResults.length === 0) return;
-    setSelected(value);
-    setInput(value);
-    setSearchOpen(false);
-  };
-
-  useEffect(() => {
-    if (!input) {
-      setSearchOpen(false);
-      setQuery("");
-      return;
-    }
-
-    if (input === selected) {
-      setSearchOpen(false);
-      return;
-    }
-
-    const delay = setTimeout(() => {
-      setQuery(input);
-      setSearchOpen(true);
-    }, 500);
-
-    return () => clearTimeout(delay);
-  }, [input]);
-
-  const placeholders: string[] = [
-    "Ducks",
-    "Radiohead",
-    "The smashing pumpkins",
-    "Cats",
-    "Edam cheese",
-    "Earth",
-    "Van Halen",
-    "Linus Torvalds",
-    "Linux kernel",
-    "Jimi Hendrix",
-    "Minecraft",
-    "Time travel",
-    "Quantum physics",
-    "Roman empire",
-    "Area 51",
-    "Evolution",
-    "Vikings",
-    "Game development",
-    "Artificial intelligence",
-    "Black holes",
-    "X Japan",
-    "Neon Genesis Evangelion",
-    "Pink Floyd",
-    "Metallica",
-    "Angine de Poitrine",
-    "Weezer",
-    "Black Sabbath",
-    "Bocchi the Rock",
-    "88Kasyo Junrei",
-  ];
-
   return (
     <div className="flex flex-col gap-18 items-center w-full md:w-2/3 lg:w-1/3 lg:min-w-140 m-auto p-8">
-      <form
-        className="w-full flex items-end gap-5"
-        onSubmit={(e) => handleSelect(searchResults[0], e)}
-      >
-        <Field>
-          <span className="flex flex-col gap-2">
-            <FieldLabel className="text-xl" htmlFor="input">
-              I want a quiz about...
-            </FieldLabel>
-            <Popover open={searchOpen} onOpenChange={setSearchOpen}>
-              <PopoverAnchor asChild>
-                <InputGroup>
-                  <InputGroupInput
-                    id="input"
-                    autoComplete="off"
-                    placeholder={`${placeholders[Math.floor(Math.random() * placeholders.length)]}...`}
-                    value={input}
-                    onChange={(e) => setInput(e.target.value)}
-                  />
-
-                  <InputGroupButton
-                    onClick={() => handleSelect(searchResults[0])}
-                  >
-                    {isSearching ? <Spinner /> : <LucideSearch />}
-                  </InputGroupButton>
-                </InputGroup>
-              </PopoverAnchor>
-
-              <PopoverContent
-                onOpenAutoFocus={(e) => e.preventDefault()}
-                className="p-0 mt-0 w-(--radix-popover-trigger-width)"
-              >
-                <AnimatePresence mode="wait">
-                  <motion.div
-                    initial={{ opacity: 0, y: -5 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.1 }}
-                    className="w-full flex flex-col rounded-lg px-1 py-2"
-                  >
-                    {searchResults && searchResults.length > 0 ? (
-                      searchResults?.map((result) => (
-                        <button
-                          className="my-1 cursor-pointer hover:bg-input h-8 px-3 rounded-lg text-left transition-colors"
-                          key={result}
-                          onClick={() => handleSelect(result)}
-                        >
-                          {result}
-                        </button>
-                      ))
-                    ) : (
-                      <span className="w-full text-center">No results.</span>
-                    )}
-                  </motion.div>
-                </AnimatePresence>
-              </PopoverContent>
-            </Popover>
-          </span>
-        </Field>
-      </form>
+      <SearchBar selected={selected} onSelect={setSelected} />
 
       <div className="border border-input bg-transparent rounded-md px-6 py-4 w-full min-h-40 flex flex-col gap-3">
         <Label className="text-2xl font-bold">Summary</Label>
