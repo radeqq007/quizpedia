25300b55eee1404532c4d909ec0ea8a526cfbb25
radeqq007 <radeqq007@interia.pl>
2026-04-30 15:26:43 +0200

    style: change the cursor to pointer on the whole answer button

--- src/components/quiz/AnswerButton.tsx
+++ src/components/quiz/AnswerButton.tsx
@@ -9,14 +9,14 @@ export const AnswerButton = ({ option, idx, onClick }: AnswerButtonProps) => {
     <button
       type="button"
       key={option}
-      className="h-full min-h-12 text-xl flex justify-start items-center group min-w-0 overflow-hidden"
+      className="h-full min-h-12 text-xl flex justify-start items-center group min-w-0 overflow-hidden cursor-pointer"
       onClick={onClick}
     >
       <span className="bg-primary text-primary-foreground font-bold text-2xl min-w-0 h-full aspect-square flex items-center justify-center rounded-l-lg shrink-0">
         {["A", "B", "C", "D"][idx]}
       </span>
 
-      <span className="border border-l-0 border-primary h-full w-full px-1 flex items-center justify-center rounded-r-lg group-hover:bg-primary/80 cursor-pointer transition-colors">
+      <span className="border border-l-0 border-primary h-full w-full px-1 flex items-center justify-center rounded-r-lg group-hover:bg-primary/80 transition-colors">
         <span className="leading-tight wrap-break-word min-w-0 w-full text-center py-2">
           {option}
         </span>
