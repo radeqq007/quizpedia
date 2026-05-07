5bf998b6d4a9952cca2f07e77c7bed869f621ad0
radeqq007 <radeqq007@interia.pl>
2026-05-08 00:56:02 +0200

    style: fix footer text aligment

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const Footer = () => {
   return (
     <div className="w-full h-10 sm:py-6 px-4 text-secondary-foreground flex flex-col-reverse sm:flex-row items-center sm:justify-between *:flex *:gap-1 sm:*:gap-4 mb-2 sm:mb-0">
-      <span className="flex flex-col-reverse sm:flex-row justify-center items-center">
+      <span className="flex flex-col-reverse justify-center items-center sm:flex-row sm:justify-start w-full">
         © 2026
         <span className="text-center">
           Made with ❤ by{" "}
@@ -18,7 +18,7 @@ export const Footer = () => {
         </span>
       </span>
 
-      <span className="w-full flex justify-center !gap-4 sm:justify-end">
+      <span className="w-full flex justify-center gap-4! sm:justify-end">
         <Link to="/privacy" className="underline">
           Privacy Policy
         </Link>
